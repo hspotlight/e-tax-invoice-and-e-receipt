e86ad26c9af74ef604f9edf9785925c1aca7671a
Pakawat Anekwiroj <pakawat.ane@gmail.com>
2023-12-27 22:04:48 +0700

    add condition and new workflow to upload s3

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 require("dotenv").config();
 const { eTaxService } = require("./etaxService");
 const { algoliaService } = require("./algoliaService");
-// const fs = require("node:fs");
+const fs = require("node:fs");
+
+const saveJson = process.env.SAVE_JSON;
 
 const handler = async () => {
   console.log("program start");
   const result = await eTaxService.getAllRecords();
   console.log("total list from etax.rd.go.th = ", result.length);
 
-  // fs.writeFile('data.json', JSON.stringify(result, null, 2), err => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  //   // file written successfully
-  // });
-
-  algoliaService
-    .replaceAllObjects(result)
-    .then(({ objectIDs }) => {
-      console.log("updated list length = ", objectIDs.length);
-      console.log("done");
-    })
-    .catch((e) => {
-      console.log("error", e);
+  if (saveJson) {
+    fs.writeFile("data.json", JSON.stringify(result, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
     });
+  } else {
+    algoliaService
+      .replaceAllObjects(result)
+      .then(({ objectIDs }) => {
+        console.log("updated list length = ", objectIDs.length);
+        console.log("done");
+      })
+      .catch((e) => {
+        console.log("error", e);
+      });
+  }
 };
 
 handler();
