bcc8a18c5a3135616d09e4e3de93e034283ecb12
Pakawat Anekwiroj <pakawat.ane@gmail.com>
2023-12-27 07:58:37 +0700

    Update eTaxService

--- etl/etaxService.js
+++ etl/etaxService.js
@@ -1,13 +1,17 @@
+const axios = require("axios");
 const eTaxRdUrl = "https://etax.rd.go.th/rd/services/rs.registeredlist/searchNew";
-// "body": "{\"pageNum\":1,\"pageSize\":10}"
 
-const getTotalSize = async () => {
-    return Promise.resolve(100);
+const eTaxApi = async (pageSize = 10) => {
+    return axios.post(eTaxRdUrl, {
+        pageNum: 1,
+        pageSize: pageSize
+    }).then(r => r.data);
 }
 
 const getAllRecords = async () => {
-    const totalSize = await getTotalSize();
-    return Promise.resolve([{}, {}]);
+    const totalSize = (await eTaxApi()).totalsize;
+    const result = await eTaxApi(totalSize);
+    return result.data;
 }
 
 const eTaxService = {
